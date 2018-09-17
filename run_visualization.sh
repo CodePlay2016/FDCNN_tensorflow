f74a93dde36ebb2223e6994582c713ba205ee105
胡方全 <hufangquan@MBP.local>
2018-09-17 16:23:01 +0800

    debug

--- run_visualization.sh
+++ run_visualization.sh
@@ -1,7 +1,7 @@
 data_dir="/home1/TestDataFromWen/arranged/steady_condition/pkl/"
 model_path="checkpoint/cvgg19/2018-09-10_163046/model.ckpt"
 debug=$1
-if [ "$debug" = true ]
+if [ "$debug" -eq true ]
 then
     python -m pdb ./visualization/test_cnn_local.py --data_dir=$data_dir\
     --model_path=$model_path
