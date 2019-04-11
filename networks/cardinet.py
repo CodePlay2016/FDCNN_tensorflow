9f9dae8b49cdd81772e0be5f08f023b6c3e9bce0
CodePlay2016 <13269308268@163.com>
2019-04-11 19:32:04 +0800

    debug

--- networks/cardinet.py
+++ networks/cardinet.py
@@ -26,11 +26,11 @@ def first_block(inpt, num_features,kernel_size,is_training,dropout_rate=0.8):
 
 def build_block(inpt, num_features, kernel_size, down_sample, is_training, name, dropout_rate=0.5):
     with tf.name_scope('buildblock'+name):
-        # feature_shape = inpt.get_shape().as_list()
-        # if feature_shape[1] == 1:
-        #     down_sample = False
-        # if feature_shape[1] < kernel_size:
-        #     kernel_size = feature_shape[1]
+        feature_shape = inpt.get_shape().as_list()
+        if feature_shape[1] == 1:
+            down_sample = False
+        if feature_shape[1] < kernel_size:
+            kernel_size = feature_shape[1]
         
         a, b = tf.constant(dropout_rate,dtype=tf.float32), tf.constant(1.0,dtype=tf.float32)
         dropout_rate = tf.cond(is_training, lambda: a, lambda: b)
@@ -42,7 +42,7 @@ def build_block(inpt, num_features, kernel_size, down_sample, is_training, name,
             out = tf.layers.batch_normalization(inpt, training=is_training)
             out = tf.nn.relu(out)
             out = tf.layers.dropout(out,dropout_rate)
-            out = tf.layers.conv1d(out,num_features,1,padding='SAME')
+            out = tf.layers.conv1d(out,num_features,kernel_size,padding='SAME')
             out = tf.layers.batch_normalization(out, training=is_training)
             out = tf.nn.relu(out)
             out = tf.layers.dropout(out,dropout_rate)
